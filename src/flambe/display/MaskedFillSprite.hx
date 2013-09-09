b23ba739f38b2eff26b9bed372d20f76230e2a64
Chris <chris@sumdog.com>
2013-09-09 10:47:25 +0100

    Remove an extraneous trace

--- src/flambe/display/MaskedFillSprite.hx
+++ src/flambe/display/MaskedFillSprite.hx
@@ -10,16 +10,16 @@ import flambe.util.Value;
 /**
  * A sprite that displays the given mask texture in the given color.
  */
-class MaskedFillSprite extends ImageSprite
-{
-    public function new (color :Int, mask :Texture)
-    {
-        var texture = System.createTexture(mask.width, mask.height);
-        texture.graphics.fillRect(color, 0, 0, mask.width, mask.height);
-        texture.graphics.setBlendMode(Mask);
-        texture.graphics.drawImage(mask, 0, 0);
+class MaskedFillSprite extends ImageSprite {
 
-		trace(texture.width, texture.height);
-        super(texture);
-    }
+  public function new (color :Int, mask :Texture) {
+
+    var texture = System.createTexture(mask.width, mask.height);
+    texture.graphics.fillRect(color, 0, 0, mask.width, mask.height);
+    texture.graphics.setBlendMode(Mask);
+    texture.graphics.drawImage(mask, 0, 0);
+    super(texture);
+
+  }
+
 }
